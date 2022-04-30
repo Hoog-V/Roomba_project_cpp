444d3b2d19194b11526fc04ba1eb6dae9706ee12
Hoog-V <1001251@hr.nl>
2022-05-01 00:33:23 +0200

    Improved variablenames and randomness in init_test.cpp

--- Roomba/tests/UART_tests/init_test.cpp
+++ Roomba/tests/UART_tests/init_test.cpp
@@ -4,43 +4,42 @@
 
 #include <gtest/gtest.h>
 #include <UART/UART.hpp>
-#include <cstdlib>
 
 TEST(UART_init, EmptyDevicePath){
-    UARTSettings p;
-    p.DevicePath = "COM1";
+    UARTSettings TestSettings;
+    TestSettings.DevicePath = "COM1";
     EXPECT_ANY_THROW({
-        UART* u = new UART(p);
+        UART* u = new UART(TestSettings);
     });
 }
 
 TEST(UART_init, EmptyBaudrate){
-    UARTSettings p;
-    p.DevicePath = "";
-    p.baudrate = UART::baud_115200;
+    UARTSettings TestSettings;
+    TestSettings.DevicePath = "";
+    TestSettings.baudrate = UART::baud_115200;
     EXPECT_ANY_THROW({
-        UART* u = new UART(p);
+        UART* u = new UART(TestSettings);
     });
 }
 
 TEST(UART_init, InvalidBaudrate){
-    UARTSettings p;
-    p.DevicePath = "COM1";
-    p.baudrate = rand() % 100; //Random number between 0 and 100
+    UARTSettings TestSettings;
+    TestSettings.DevicePath = "COM1";
+    TestSettings.baudrate = std::rand() % 100; //Random number between 0 and 100
     EXPECT_ANY_THROW({
-        UART* u = new UART(p);
+        UART* u = new UART(TestSettings);
     });
-    p.baudrate = UART::baud_115200;
+    TestSettings.baudrate = UART::baud_115200;
     EXPECT_NO_THROW({
-        UART* u = new UART(p);
+        UART* u = new UART(TestSettings);
     });
 }
 
 TEST(UART_init, ValidUartSettings){
-    UARTSettings p;
-    p.DevicePath = "COM1";
-    p.baudrate = UART::baud_115200;
+    UARTSettings TestSettings;
+    TestSettings.DevicePath = "COM1";
+    TestSettings.baudrate = UART::baud_115200;
     EXPECT_NO_THROW({
-        UART* u = new UART(p);
+        UART* u = new UART(TestSettings);
     });
 }
