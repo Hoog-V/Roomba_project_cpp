815211fdcd29f4e984c6002f3ce93082cbec7386
Hoog-V <1001251@hr.nl>
2022-05-01 00:19:33 +0200

    Added tests for sendbytes method of UART class

---
+++ Roomba/tests/UART_tests/SendBytes_test.cpp
@@ -0,0 +1,32 @@
+//
+// Created by victor on 1-5-22.
+//
+#include <gtest/gtest.h>
+#include <UART/UART.hpp>
+#include <array>
+
+TEST(UART_SendBytes, OutOfBoundsCheck)
+{
+    UARTSettings TestSettings;
+    TestSettings.baudrate = UART::baud_115200;
+    TestSettings.DevicePath = "/dev/ttyS0";
+    UART* p = new UART(TestSettings);
+    std::array <uint8_t, 10>testarr;
+    testarr.fill(0);
+    EXPECT_ANY_THROW({
+       p->sendBytes(testarr, 20);
+    });
+}
+
+TEST(UART_SendBytes, numOfBytesZero)
+{
+    UARTSettings TestSettings;
+    TestSettings.baudrate = UART::baud_115200;
+    TestSettings.DevicePath = "/dev/ttyS0";
+    UART* p = new UART(TestSettings);
+    std::array <uint8_t, 10>testarr;
+    testarr.fill(0);
+    EXPECT_ANY_THROW({
+        p->sendBytes(testarr, 0);
+    });
+}
