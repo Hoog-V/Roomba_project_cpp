57cd79c3c7874c687e30e5d8de7caaf217bb1217
Hoog-V <1001251@hr.nl>
2022-05-01 00:27:41 +0200

    Added tests for readbytes method of UART class

---
+++ Roomba/tests/UART_tests/ReadBytes_test.cpp
@@ -0,0 +1,32 @@
+//
+// Created by victor on 1-5-22.
+//
+#include <gtest/gtest.h>
+#include <UART/UART.hpp>
+
+
+
+TEST(UART_ReadBytes, OutOfBoundsCheck)
+{
+    UARTSettings TestSettings;
+    TestSettings.baudrate = UART::baud_115200;
+    TestSettings.DevicePath = "/dev/ttyS0";
+    UART* p = new UART(TestSettings);
+    std::array <uint8_t, 10>testarr;
+    const uint8_t OutOfBoundsVal = 2*testarr.size();
+    EXPECT_ANY_THROW({
+        p->readBytes(testarr, OutOfBoundsVal);
+    });
+}
+
+TEST(UART_ReadBytes, numOfBytesZero)
+{
+    UARTSettings TestSettings;
+    TestSettings.baudrate = UART::baud_115200;
+    TestSettings.DevicePath = "/dev/ttyS0";
+    UART* p = new UART(TestSettings);
+    std::array <uint8_t, 10>testarr;
+    EXPECT_ANY_THROW({
+        p->readBytes(testarr, 0);
+    });
+}
