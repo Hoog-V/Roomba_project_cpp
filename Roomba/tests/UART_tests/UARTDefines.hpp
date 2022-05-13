ac494d5ad63efe9521c740e118163ab6ac56a901
Hoog-V <1001251@hr.nl>
2022-05-13 22:32:00 +0200

    Added standard dummysocket for socat emulation

--- Roomba/tests/UART_tests/UARTDefines.hpp
+++ Roomba/tests/UART_tests/UARTDefines.hpp
@@ -3,7 +3,7 @@
 
 #include <UART/UART.hpp>
 
-const char TestPath[] = "/dev/null"; //standard path for linux (virtual file descriptor)
+const char TestPath[] = "/dev/pts/2"; //standard path for linux (virtual file descriptor)
 const baudrates TestBaud = baudrates::baud_115200;
 
 #endif
