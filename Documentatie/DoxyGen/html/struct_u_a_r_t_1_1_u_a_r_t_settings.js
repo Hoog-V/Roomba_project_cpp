d44d9489f404a8ba42c2f6459bc1b7614e583b63
Casperdroid5 <caspertak1@gmail.com>
2022-05-19 12:57:55 +0200

    -enh wachtwoorden en naam RPI toegevoegd.

---
+++ Documentatie/DoxyGen/html/struct_u_a_r_t_1_1_u_a_r_t_settings.js
@@ -0,0 +1,5 @@
+var struct_u_a_r_t_1_1_u_a_r_t_settings =
+[
+    [ "Baudrate", "struct_u_a_r_t_1_1_u_a_r_t_settings.html#ac45ca1e512e65be605cddb15a2b9d2f0", null ],
+    [ "DevicePath", "struct_u_a_r_t_1_1_u_a_r_t_settings.html#ab0a07f52faffef3ab4e9dc65a680f270", null ]
+];
