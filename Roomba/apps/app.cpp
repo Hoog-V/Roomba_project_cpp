701bc8e354d1d96d7d4f27e418a68013943be450
zainSabbagh <99176156+zainSabbagh@users.noreply.github.com>
2022-06-03 14:05:10 +0200

    -add turnon function

--- Roomba/apps/app.cpp
+++ Roomba/apps/app.cpp
@@ -9,13 +9,15 @@
 int main(int argc, char **argv)
 {
 	UART::UARTSettings p;
-	p.DevicePath = "COM3";
+	p.DevicePath = "COM6";
 	p.Baudrate = UART::Baudrates::baud_115200;
 	UART::UART * p1 = new UART::UART(p);
 	Roomba::Roomba* rmb = new Roomba::Roomba(p1);
 	//rmb->setDockMode();
-	rmb->setLed(Roomba::leds::LED_Dock, Roomba::LedState::On);
-	rmb->setControlMode(Roomba::control::Passive);
+	//rmb->setLed(Roomba::leds::LED_Dock, Roomba::LedState::On);
+	//rmb->setControlMode(Roomba::control::Passive);
+    rmb->turnOn();
+
 	delete p1;
 	return (0);
 }
