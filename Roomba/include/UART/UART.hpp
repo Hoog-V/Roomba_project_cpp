e4ea43fb76a81bd466c7cf26f817ec425811b73e
Victor Hogeweij <hogeweyv@gmail.com>
2022-06-08 20:13:29 +0200

    -enh was niet consequent met pragma once

--- Roomba/include/UART/UART.hpp
+++ Roomba/include/UART/UART.hpp
@@ -1,10 +1,3 @@
-//
-// Created by victor on 8-6-22.
-//
-
-#ifndef TESTPROJ_UART_H
-#define TESTPROJ_UART_H
+#pragma once
 #include <UART/UARTBase.hpp>
 #include <UART/PC/UARTPC.hpp>
-
-#endif //TESTPROJ_UART_H
