217b1506d158cedb686dfc396bf8a8672c4639b6
Thomas <thomasvdoever@xs4all.nl>
2022-06-22 17:38:53 +0200

    -add examples folder met SimpleController.cpp

---
+++ apps/examples/SimpleController.cpp
@@ -0,0 +1,16 @@
+#include <UART/UART.hpp>
+#include <array>
+#include "Roomba/Roomba.hpp"
+
+
+int main(int argc, char **argv) {
+	UART::UARTSettings p;
+	p.devicePath = "/dev/ttyUSB0";
+	p.baudrate = UART::Baudrates::baud_115200;
+    p.connectionMethod = UART::connectionMethod::USB;
+	std::shared_ptr<UART::UART> p1 = UART::UART::Create(p);
+    std::unique_ptr<Roomba::Roomba> rmb = std::make_unique<Roomba::Roomba>(p1);
+
+    
+	return (0);
+}
